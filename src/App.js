e967c2ce7e019fd21f92d8ad7c74e29759880821
Justin Chang <changguangyu@yahoo.com>
2020-12-30 19:05:31 -0800

    Finish parallax layers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,106 @@
-import Ocean from "./components/Ocean";
 import Bio from "./components/Bio";
+import Hill from "./components/ocean/Hill";
+import Icons from "./components/Icons";
+import Lights from "./components/ocean/Lights";
+import Water from "./components/ocean/Water";
+
+import { useState, useEffect } from "react";
+import Parallax from "react-rellax";
+
+const currentWidth = () => {
+    return (
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth
+    );
+};
+
+const currentHeight = () => {
+    return (
+        window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.body.clientHeight
+    );
+};
 
 function App() {
+    let [width, setWidth] = useState(currentWidth());
+    let [height, setHeight] = useState(currentHeight());
+
+    useEffect(() => {
+        let widthTimeoutId = null;
+        let heightTimeoutId = null;
+        const resizeListener = () => {
+            clearTimeout(widthTimeoutId);
+            clearTimeout(heightTimeoutId);
+            widthTimeoutId = setTimeout(() => setWidth(currentWidth()), 144);
+            heightTimeoutId = setTimeout(() => setHeight(currentHeight()), 144);
+        };
+        window.addEventListener("resize", resizeListener);
+
+        return () => {
+            window.removeEventListener("resize", resizeListener);
+        };
+    }, []);
+
     return (
-        <div className="app">
-            <Ocean />
-            <Bio />
+        <div className="ocean">
+            <Parallax speed={-15}>
+                <Water width={width} height={height} />
+            </Parallax>
+
+            <Parallax speed={-12}></Parallax>
+
+            <Parallax speed={-9}>
+                <Hill
+                    width={width}
+                    height={height}
+                    imgLink="https://i.imgur.com/WMmmbjC.png"
+                    color="#4DB9E7"
+                    start={0.4}
+                />
+            </Parallax>
+
+            <Parallax speed={-6}>
+                <Hill
+                    width={width}
+                    height={height}
+                    imgLink="https://i.imgur.com/6qSjN5g.png"
+                    color="#2FA1D7"
+                    start={0.5}
+                />
+            </Parallax>
+
+            <Parallax className="ocean-name-container" speed={-12}>
+                <div className="ocean-title">JUSTIN</div>
+                <div className="ocean-title">CHANG</div>
+                <Icons />
+            </Parallax>
+
+            <Parallax speed={-3}>
+                <Hill
+                    width={width}
+                    height={height}
+                    imgLink="https://i.imgur.com/l9urhcC.png"
+                    color="#0272BA"
+                    start={0.6}
+                />
+            </Parallax>
+
+            <Parallax speed={3}>
+                <Hill
+                    width={width}
+                    height={height}
+                    imgLink="https://i.imgur.com/9uymSl7.png"
+                    color="#E8CA84"
+                    start={0.8}
+                />
+            </Parallax>
+
+            <Parallax speed={3}>
+                <Lights width={width} height={height} />
+                <Bio />
+            </Parallax>
         </div>
     );
 }
--- src/components/ocean/Hill.js
+++ src/components/ocean/Hill.js
@@ -1,29 +1,29 @@
 import { useEffect, useRef } from "react";
 
-function Hill2(props) {
-    const hill2Ref = useRef(null);
+function Hill(props) {
+    const hillRef = useRef(null);
 
     const drawHill = (ctx) => {
         ctx.clearRect(0, 0, props.width, props.height);
 
         const hill = new Image();
-        hill.src = "https://i.imgur.com/IuNL94m.png";
+        hill.src = props.imgLink;
         hill.onload = () => {
             let pattern = ctx.createPattern(hill, "repeat");
             ctx.fillStyle = pattern;
 
             let fillStart = 0;
-            while (fillStart < ctx.height * 0.5) {
+            while (fillStart < ctx.height * props.start) {
                 fillStart += hill.height;
             }
             ctx.fillRect(-330, fillStart, ctx.width * 2, hill.height);
-            ctx.fillStyle = "#2FA1D7";
-            ctx.fillRect(0, fillStart + hill.height, ctx.width, props.height);
+            ctx.fillStyle = props.color;
+            ctx.fillRect(0, fillStart + hill.height, ctx.width, ctx.height);
         };
     };
 
     useEffect(() => {
-        let ctx = hill2Ref.current.getContext("2d");
+        let ctx = hillRef.current.getContext("2d");
         ctx.width = props.width;
         ctx.height = props.height;
         drawHill(ctx);
@@ -31,12 +31,12 @@ function Hill2(props) {
 
     return (
         <canvas
-            ref={hill2Ref}
-            id="hill2"
+            ref={hillRef}
+            id="hill"
             width={window.innerWidth}
             height={window.innerHeight}
         />
     );
 }
 
-export default Hill2;
+export default Hill;
